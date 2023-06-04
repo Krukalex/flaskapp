968264c7fe5492b94424f8c7b40ded58d51b652c
Alex Kruk <113789090+Krukalex@users.noreply.github.com>
2023-06-04 18:59:07 -0400

    Update flaskblog.py

--- flask app/flaskblog.py
+++ flask app/flaskblog.py
@@ -76,6 +76,6 @@ def login():
     return render_template('login.html', title = 'Login', form = form)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80)
+    app.run(host = '0.0.0.0', port = 5000)
     #serve(app, host = '0.0.0.0', port = 80)
 
